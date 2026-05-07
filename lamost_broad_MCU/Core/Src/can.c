449969cb22eef8a6dfe9a4107e8bf1998052dd03
dushiyu <820145912@qq.com>
2026-05-07 15:05:05 +0800

    GPIO_AF9_CAN引脚复用

--- lamost_broad_MCU/Core/Src/can.c
+++ lamost_broad_MCU/Core/Src/can.c
@@ -80,7 +80,7 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF9_TIM1;
+    GPIO_InitStruct.Alternate = GPIO_AF9_CAN;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* USER CODE BEGIN CAN_MspInit 1 */

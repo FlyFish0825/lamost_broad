63b36d05d698b33ffdf48cccdfba19e7cef3e0da
dushiyu <820145912@qq.com>
2026-05-09 20:47:48 +0800

    添加LED相关文件

---
+++ lamost_broad_MCU/Core/pcb_broad/just_led.c
@@ -0,0 +1,30 @@
+#include "just_led.h"
+#include "main.h"
+
+
+const uint16_t pcb_1_map[16] = {0x0201, 0x0202, 0x0203, 0x0101, 0x0204, 0x0102,
+                                0x0205, 0x0103, 0x0301, 0x0302, 0x0303, 0x0304,
+                                0x0305, 0x0306, 0x0307, 0x0308};
+
+
+
+
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    if (htim->Instance == TIM1) {
+        // 在这里处理 TIM1 的周期性中断事件
+        // 例如，切换 LED 状态或执行其他定时任务
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET); // 示例：熄灭 LED
+    }
+}                                
+
+void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    if (htim->Instance == TIM1) {
+        // 在这里处理 TIM1 的输出比较中断事件
+        // 例如，切换 LED 状态或执行其他定时任务
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET); // 示例：点亮 LED
+    }
+}
+
---
+++ lamost_broad_MCU/Core/pcb_broad/just_led.h
@@ -0,0 +1,11 @@
+#ifndef __JUST_LED_H
+#define __JUST_LED_H
+
+#include "main.h"
+
+extern const uint16_t pcb_1_map[16];
+
+
+
+
+#endif /* __JUST_LED_H */

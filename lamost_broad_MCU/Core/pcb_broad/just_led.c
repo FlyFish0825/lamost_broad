ac04a7e4249361773864edb7623e5aab6a20b8ea
dushiyu <820145912@qq.com>
2026-05-09 21:14:43 +0800

    10%占空比亮

--- lamost_broad_MCU/Core/pcb_broad/just_led.c
+++ lamost_broad_MCU/Core/pcb_broad/just_led.c
@@ -9,13 +9,12 @@ const uint16_t pcb_1_map[16] = {0x0201, 0x0202, 0x0203, 0x0101, 0x0204, 0x0102,
 
 
 
-
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
     if (htim->Instance == TIM1) {
         // 在这里处理 TIM1 的周期性中断事件
         // 例如，切换 LED 状态或执行其他定时任务
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET); // 示例：熄灭 LED
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET); // 点亮LED
     }
 }                                
 
@@ -24,7 +23,7 @@ void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim)
     if (htim->Instance == TIM1) {
         // 在这里处理 TIM1 的输出比较中断事件
         // 例如，切换 LED 状态或执行其他定时任务
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET); // 示例：点亮 LED
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET); // 熄灭LED
     }
 }
 
--- lamost_broad_MCU/Core/pcb_broad/just_led.h
+++ lamost_broad_MCU/Core/pcb_broad/just_led.h
@@ -7,5 +7,8 @@ extern const uint16_t pcb_1_map[16];
 
 
 
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim);
+void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim);
+
 
 #endif /* __JUST_LED_H */
